9f36add2e96bd955777d468981ac9c4aebc3ee18
phat2541 <weeraphatphat3@gmail.com>
2025-04-23 16:40:49 +0700

    csz

--- app.js
+++ app.js
@@ -10,8 +10,18 @@ const PORT = 3000;
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
-app.set("views","./src/views");
+app.set("views","../src/views");
 app.set("view engine","ejs")
+
+productRouter.router("/").get((req,res) =>{
+    res.render("products");
+});
+
+productRouter.router("/1").get((req,res) =>{
+    res.send("hello 1");
+});
+
+app.use("/products", productRouter)
 
 app.get("/", (req,res) =>{
 
